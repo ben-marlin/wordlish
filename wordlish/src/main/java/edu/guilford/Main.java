0142203f5af2b18f53378413c8afb0dac2a3ac82
ben-marlin <bmarlin@guilford.edu>
2024-08-31 14:55:45 -0400

    sample code

--- wordlish/src/main/java/edu/guilford/Main.java
+++ wordlish/src/main/java/edu/guilford/Main.java
@@ -1,10 +1,6 @@
 package edu.guilford;
 
-import java.util.Scanner;
-import java.util.Random;
-
 public class Main {
-
     // ANSI escape codes for text colors
     private static final String RESET = "\u001B[0m";
     private static final String BLACK = "\u001B[30m";
@@ -43,13 +39,10 @@ public class Main {
             "build", "bring", "cover", "grove", "slant"
         };
 
-        Random rand = new Random();
+        // choose one of the words
+        String chosenWord = words[0].toUpperCase();
 
-        // establish word to copy
-        String chosenWord = words[rand.nextInt(100)].toUpperCase();
-        char[] chosenArray = chosenWord.toCharArray();
-
-/*         // create blank string for printing
+        // create blank string for printing
         String colorExample = new String();
         
         // make string w/ alternating colors, space b/w letters
@@ -60,80 +53,7 @@ public class Main {
         colorExample += YELLOW + chosenWord.charAt(4) + " ";
         colorExample += RESET;
 
-        System.out.println(colorExample); */
-
-        // declare color code & guess variables
-        String[] colorCodes = {RESET + " ", RESET + " ", RESET + " ", RESET + " ", RESET + " "};
-        String guessWord = new String();
-        char[] guessArray = new char[5];
-        String printWord = new String();
-
-        // instantiate a scanner
-        Scanner scan = new Scanner(System.in);
-
-        // loop for six repetitions
-        for (int k = 0; k < 6; k++) {
-            // make array of reset color codes
-            for (int i = 0; i < 5; i++) {
-                colorCodes[i] = RESET + " ";
-            }
-
-            // establish word to compare
-            System.out.print("A 5-letter word was chosen. What is your guess? ");
-            guessWord = scan.next().toUpperCase();
-            
-            while (guessWord.length() != 5) {
-                System.out.println("Your guess didn't have 5 letters!");
-                System.out.print("Guess again. ");
-                guessWord = scan.next().toUpperCase();
-            }
-
-            guessArray = guessWord.toCharArray();
-
-            // nested loop compares guess to answer, red if match
-            for (int i = 0; i < 5; i++) {
-                for (int j = 0; j < 5; j++) {
-                    if (chosenArray[i] == guessArray[j]) {
-                        colorCodes[j] = RED + " ";
-                    }
-                }
-            }
-
-            // loop compares guess to answer, green if match
-            for (int i = 0; i < 5; i++) {
-                if (chosenArray[i] == guessArray[i]) {
-                    colorCodes[i] = GREEN + " ";
-                }
-            }
-
-            // build a word to be printed with colors from colorCodes
-            printWord = new String();
-
-            // loop builds string alternate code & letters
-            for (int i = 0; i < 5; i++) {
-                printWord += colorCodes[i] + guessArray[i];
-            }
-
-            // reset at end of word
-            printWord += RESET;
-
-            // prints guess - yellow if in word, green if position correct
-            System.out.println(printWord);
-
-             // exit loop on correct guess
-            if (guessWord.equals(chosenWord)) {
-                break;
-            }
-        }
-
-        // message given on correct guess
-        if (guessWord.equals(chosenWord)) {
-            System.out.println("You were right!");
-        } else {
-            System.out.println("The chosen word was " + chosenWord + ".");
-        }
-
-        scan.close();
+        System.out.println(colorExample);
     }
 
 }
